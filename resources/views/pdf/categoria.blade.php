c9975058e10b9caf495f15f3d73e7de45de69475
Jeffer Lindao <jefferlindao@gmail.com>
2018-02-17 10:18:26 -0500

    v2

--- resources/views/pdf/categoria.blade.php
+++ resources/views/pdf/categoria.blade.php
@@ -1,1 +1,29 @@
-categoria.blade.php
+@extends('layouts.pdf')
+@section('contenido')
+<h3 class="text-title text-center">Lista de Categoria</h3>
+<div class="row">
+	<div class="col-xs-12">
+		<div class="table-responsive">
+			<table class="table table-bordered table-condensed table-striped">
+				<thead>
+					<tr class="text-center">
+						<th>Id</th>
+						<th>Nombre</th>
+						<th>Descripción</th>
+						
+					</tr>
+				</thead>
+				<tbody>
+					@foreach ($categorias as $cat) 
+				    <tr>
+				      <td>{{ $cat->Cate_codi }}</td>
+				      <td>{{ $cat->Cate_nomb }}</td>
+				      <td>{{ $cat->Cate_desc }}</td>
+					</tr>
+					@endforeach
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
+@stop
--- resources/views/pdf/proveedor.blade.php
+++ resources/views/pdf/proveedor.blade.php
@@ -1,1 +1,36 @@
-proveedor.blade.php
+@extends('layouts.pdf')
+@section('contenido')
+<h3 class="text-title text-center">Lista de Proveedores</h3>
+<div class="row">
+	<div class="col-xs-12">
+		<div class="table-responsive">
+			<table class="table table-bordered table-condensed table-striped">
+				<thead>
+					<tr class="text-center">
+						<th>Id</th>
+						<th>Nombre</th>
+						<th>Dirección</th>
+						<th>TipoDoc.</th>
+						<th>NumeroDoc.</th>
+						<th>Teléfono</th>
+						<th>Email</th>
+					</tr>
+				</thead>
+				<tbody>
+					@foreach ($proveedores as $pro) 
+				    <tr>
+				      <td>{{ $pro->Pers_codi }}</td>
+				      <td>{{ $pro->Pers_nomb }}</td>
+				      <td>{{ $pro->Pers_dire }}</td>
+				      <td>{{ $pro->Pers_tido }}</td>
+				      <td>{{ $pro->Pers_nudo }}</td>
+				      <td>{{ $pro->Pers_tele }}</td>
+				      <td>{{ $pro->Pers_emai }}</td>
+					</tr>
+					@endforeach
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
+@stop
